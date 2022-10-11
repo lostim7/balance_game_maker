0f80a022c5b0eefa5a8c8e8fa25b2b1b2d02f261
lostim7 <lostim7428@naver.com>
2022-10-11 17:05:17 +0900

    아직 한 거 없음

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const menus = ['찾아보기', '만들기', '고객지원']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>밸런스 게임 메이커</span> <span></span>
     </div>
-  );
+  )
 }
 
 export default App;
