1cab36c5ad696fa73716d4d827399ab89cebd3ed
lostim7 <lostim7428@naver.com>
2022-10-12 16:44:58 +0900

    ssdfsfds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 function App() {
   const menus = ['찾아보기', '만들기', '고객지원']
   return (
-    <div>
-      <span>밸런스 게임 메이커</span> <span></span>
-    </div>
+    <section>
+      <div className="main">
+        <h1>밸런스 게임을 만들어보세요!</h1><br></br>
+          <div>
+            <a className="btn btn-secondary gobutton" href="#" role="button">만들러 가기</a>
+          </div>
+      </div>
+      <div className="main">
+        <h1>밸런스 게임을 만들어보세요!</h1><br></br>
+          <div>
+            <a className="btn btn-secondary gobutton" href="#" role="button">만들러 가기</a>
+          </div>
+      </div>
+    </section>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './routes/Navbar';
+import "./Home.css"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>
 );
