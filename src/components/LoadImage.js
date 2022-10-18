4a169ea4e9d3fd5782c92080e0bcf072acca443a
lostim7 <lostim7428@naver.com>
2022-10-18 16:57:07 +0900

    생각해보니 배열 따로 저장해두기도 함 ㅎ

--- src/components/LoadImage.js
+++ src/components/LoadImage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 function LoadImage() {
   const [img, setImg] = useState('');
   const [res, setRes] = useState([]);
-  const access_Key = process.env.REACT_APP_API_KEY;  //이 부분은 여러분의 액세스키 값으로 바꿔야함.
+  const access_Key = process.env.REACT_APP_API_KEY;  //이 부분은 여러분의 액세스키 값으로 바꿔야함
 
   console.log("access key" , access_Key, process.env.REACT_APP_API_KEY)
   const url = `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${access_Key}&orientation=landscape&per_page=10`;
