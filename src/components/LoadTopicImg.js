35ce9e6f931103048a5181edb0a6a527ec97fc93
lostim7 <lostim7428@naver.com>
2022-10-26 17:01:43 +0900

    api Research, 정보 뭐받아오는지 알아냄 결론:코드친거는 없음

--- src/components/LoadTopicImg.js
+++ src/components/LoadTopicImg.js
@@ -2,14 +2,16 @@ import axios from "axios";
 import { useState } from "react";
 
 function LoadTopicImg(topic) {
-    const [img, setImg] = useState('');
+    const [topic, setTopic] = useState('');
     const [res, setRes] = useState([]);
     const access_Key = process.env.REACT_APP_API_KEY;  //API 키값(.env)
-    const url = `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${access_Key}&orientation=landscape&per_page=2`
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${topic}&client_id=${access_Key}&orientation=landscape&per_page=2`
     axios.get(`${url}`).then((Response) =>
     console.log(Response))
     return (
-        <div></div>
+        <div>
+            {res.map}
+        </div>
     )
 }
 
--- src/routes/Play.js
+++ src/routes/Play.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
 import LoadTopicImg from "../components/LoadTopicImg";
-import PlayID from "../components/PlayID";
 
 
 function Play() {
     return
 }   
+
+LoadTopicImg()
 
 export default Play;
